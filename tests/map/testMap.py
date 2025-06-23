b381066ad32dc5eb6e535350602277cbcf81ad68
DiegoAlfaroUCR <diego.alfarosegura@ucr.ac.cr>
2025-06-22 22:12:13 -0600

    Agregar configuración para pruebas de flake8 con docstrings.
    Se agrega un archivo de configuración para uso de flake8, que permite detectar errores de formato en los docstrings.
    
    Signed-off-by: DiegoAlfaroUCR <diego.alfarosegura@ucr.ac.cr>

--- tests/map/testMap.py
+++ tests/map/testMap.py
@@ -2,13 +2,12 @@ import csv
 import time
 import sys
 import os
+from src import apiManager
 
 # Cambiar directorio de imports para agregar apiManager
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__),
                                             '..', '..'))
 sys.path.insert(0, project_root)
-
-from src import apiManager
 
 datos_path = 'tests/map/datos_gps.csv'
 
